fffc9d8bf03f168e647114ddc78b87f77b27e830
jhonntantb <jhonntantb@gmail.com>
2024-08-14 19:49:10 -0300

    Add initial configuration to project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Carga de estudiantes</h1>
+      <form action=''></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Card = () => {
+  return <div>Hola, soy una tarjeta </div>;
+};
+
+export default Card;
