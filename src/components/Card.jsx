f736c6f323a19cb8bf6d466a5e0148885fd98bb5
jhonntantb <jhonntantb@gmail.com>
2024-08-14 22:19:06 -0300

    Add styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Card = ({ name, book }) => {
   console.log(name, book);
   return (
-    <div>
-      <p>Hola {name}</p>
+    <div className='card'>
+      <p>Hola {name}!</p>
       <p>Sabemos que uno de tus libros favoritos es:</p>
-      <p>{book}</p>
+      <p className='response'>{book}</p>
     </div>
   );
 };
