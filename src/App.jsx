5e8d7d2be4389c032abf4d66bdfa18de98c695bd
jhonntantb <jhonntantb@gmail.com>
2024-08-14 21:53:49 -0300

    Add logic to functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,71 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
+  const [formValues, setFormValues] = useState({ name: '', book: '' });
+  const [submittedValues, setSubmittedValues] = useState(null);
+  const [showError, setShowError] = useState(false);
+
+  const handleInput = (e) => {
+    const { id, value } = e.target;
+    setSubmittedValues(null);
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [id]: value,
+    }));
+  };
+
+  const validateFirstInput = (value) => value.trimEnd().length >= 3;
+
+  const validateSecondInput = (value) => value.length >= 6;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, book } = formValues;
+
+    if (validateFirstInput(name) && validateSecondInput(book)) {
+      setSubmittedValues({ name, book });
+      setFormValues({ name: '', book: '' });
+      setShowError(false);
+    } else {
+      setShowError(true);
+    }
+  };
   return (
     <div className='app'>
-      <h1>Carga de estudiantes</h1>
-      <form action=''></form>
-      <Card />
+      <h1>Agrega un libro</h1>
+      <form
+        onSubmit={handleSubmit}
+        className='form'
+      >
+        <input
+          type='text'
+          value={formValues.name}
+          id='name'
+          placeholder='Ingresa tu nombre'
+          onChange={handleInput}
+        />
+        <input
+          type='text'
+          id='book'
+          value={formValues.book}
+          placeholder='Ingrega el nombre de tu libro'
+          onChange={handleInput}
+        />
+        <button type='submit'>Enviar</button>
+      </form>
+      {showError && (
+        <p className='error'>
+          Por favor chequea que la información sea correcta
+        </p>
+      )}
+      {submittedValues && (
+        <Card
+          name={submittedValues.name}
+          book={submittedValues.book}
+        />
+      )}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta </div>;
+const Card = ({ name, book }) => {
+  console.log(name, book);
+  return (
+    <div>
+      <p>Hola {name}</p>
+      <p>Sabemos que uno de tus libros favoritos es:</p>
+      <p>{book}</p>
+    </div>
+  );
 };
 
 export default Card;
